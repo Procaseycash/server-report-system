370af6da22accf683c8cb2ff5a57de8f1b0cd4a0
Kazeem <kezyolanipekun@gmail.com>
2020-12-19 04:30:14 +0100

    created controllers and server.js setup

---
+++ server/controllers/ReportController.js
@@ -0,0 +1,27 @@
+const ResponseHandler = require( '../dtos/ResponseHandler' );
+const ReportService = require( '../services/apis/ReportService' );
+const messages = require( '../configs/messages' );
+
+class ReportController {
+
+    static async getReportStatus(req, res) {
+       try {
+           const data = await ReportService.getReportStatus(req.param.id);
+           ResponseHandler.success(res, data, messages.reportStatus);
+       } catch (e) {
+           ResponseHandler.error(res, e.message);
+       }
+    }
+
+    static async getReports(req, res) {
+        try {
+            const data = await ReportService.getReports();
+            ResponseHandler.success(res, data, messages.report);
+        } catch (e) {
+            ResponseHandler.error(res, e.message);
+        }
+    }
+
+}
+
+module.exports = ReportController;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const AppRoute = require('./routes');
+const ApiHandlerService = require( './services/ApiHandlerService');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist/'))); // make jquery available on public client app
+app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/'))); // make bootstrap available on public client app
+app.use(express.static(path.join(__dirname, '../public')));
+
+AppRoute.init(app); // initialize app routing
+
+const bootstrap = () => {
+    try {
+        ApiHandlerService.getAccessToken(); // initialize server access token
+    } catch ( e ) {
+        console.log(`TOKEN_GENERATION_AT_LAUNCH_FAILED: ${e.message}`);
+    }
+    return app;
+};
+
+module.exports = bootstrap();
